c436b2be3fb2e9a763897f2e34866c4a23941ff0
SharaawyMohamed <sharawem585@gmail.com>
2025-06-15 19:28:21 +0300

    done

---
+++ Web.APIs/Web.Application/Features/Reviews/Commands/DeleteReview/DeleteReviewCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Application.Features.Reviews.Commands.AddReview;
+using Web.Application.Helpers;
+using Web.Application.Response;
+using Web.Domain.Entites;
+using Web.Domain.Repositories;
+
+namespace Web.Application.Features.Reviews.Commands.DeleteReview
+{
+	public class DeleteReviewCommandHandler : IRequestHandler<DeleteReviewCommand, BaseResponse<int>>
+	{
+		private readonly UserManager<AppUser> _userManager;
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly IHttpContextAccessor _contextAccessor;
+
+		public DeleteReviewCommandHandler(UserManager<AppUser> userManager, IUnitOfWork unitOfWork, IHttpContextAccessor contextAccessor)
+		{
+			_userManager = userManager;
+			_unitOfWork = unitOfWork;
+			_contextAccessor = contextAccessor;
+		}
+
+		public async Task<BaseResponse<int>> Handle(DeleteReviewCommand request, CancellationToken cancellationToken)
+		{
+			if (request.Id== null)
+			{
+				return new BaseResponse<int>(false, "Invalid Id");
+			}
+
+			var user = await GetUser.GetCurrentUserAsync(_contextAccessor, _userManager);
+			if (user == null)
+			{
+				return new BaseResponse<int>(false, "User UnAuthorized!");
+			}
+
+			var review = await _unitOfWork.Repository<int, PropertyReview>().GetByIdAsync(request.Id);
+			if (review == null)
+			{
+				return new BaseResponse<int>(false, "Review not found!");
+			}
+
+			if(review.UserId != user.Id)
+			{
+				return new BaseResponse<int>(false, "You not have permission for removing this review!");
+			}
+
+			_unitOfWork.Repository<int, PropertyReview>().Remove(review);
+			await _unitOfWork.SaveChangesAsync();
+
+			return new BaseResponse<int>(true, "Your review removed successfully!");
+		}
+	}
+}
---
+++ Web.APIs/Web.Application/Features/Reviews/Commands/UpdateReview/UpdateReviewCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Application.Features.Reviews.Commands.UpdateReview
+{
+	public class UpdateReviewCommandValidator:AbstractValidator<UpdateReviewCommand>
+	{
+		public UpdateReviewCommandValidator()
+		{
+			RuleFor(x => x.Id).NotNull().GreaterThan(0);
+			RuleFor(x => x.Comment).NotNull().Length(3, 600);
+			RuleFor(x => x.Stars).NotNull().GreaterThan(0).LessThanOrEqualTo(5);
+		}
+	}
+}
